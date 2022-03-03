197c51990a061043c196e38439b44749fa838ddd
justcow <68345611+JustCoww@users.noreply.github.com>
2022-03-03 10:13:51 +0000

    Update makethumb.py

--- slowedvideos/makeimages/makethumb.py
+++ slowedvideos/makeimages/makethumb.py
@@ -5,38 +5,38 @@ def makethumb(cover, output):
     from PIL import Image, ImageDraw, ImageEnhance, ImageFilter 
     from math import trunc
 
-    # Import
+    # Sizes
+    x, y = (626, 626) # Cover size
+    X, Y = (1920, 1080) # Background size
+    
+    # Get the middle coordinates of the image
+    middle = (trunc((X-x)/2), trunc((Y-y)/2))
+    
+    # Import cover
     print(prefix, f'Importing {cover} ...')
     tb = Image.open(cover)
     cv = Image.open(cover)
 
-    # Cover
-    print(prefix, 'Resizing cover...')
-    x, y = (626, 626)
-    cv = cv.resize((x, y), resample=0, box=None)
-    
-    # Thumb Resize
-    print(prefix, 'Resizing background...')
-    X, Y = (1920, 1080)
-    tb = tb.resize((X, Y), resample=0, box=None)
-    
-    # Get the middle x y
-    middle = (trunc((X-x)/2), trunc((Y-y)/2))
+    # Resizing
+    print(prefix, 'Resizing cover and background...')
+    cv = cv.resize((x, y))
+    tb = tb.resize((X, Y))
     
     # Add a shadow (Black square with blur)
     print(prefix, 'Creating the shadow...')
     square = Image.new(mode = "RGBA", size = (x, y), color = (0, 0, 0))
     tb.paste(square, middle)
 
-    # Blur and brightness
-    print(prefix, 'Bluring background...')
+    # Blur and turn brightness down
+    print(prefix, 'Adding blur to background...')
     tb = ImageEnhance.Brightness(tb).enhance(0.4)
     tb = tb.filter(ImageFilter.GaussianBlur(25))
-    tb = tb.copy()
 
-    # Mix into file
-    print(prefix, 'Merging cover to background...')
+    # Paste cover into the background
+    print(prefix, 'Merging cover with background...')
     tb.paste(cv, middle)
+    
+    # Export final thing to a file
     print(prefix, f'Exporting thumbnail as {output} ...')
     tb.save(output)
     
