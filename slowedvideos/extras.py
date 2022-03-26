9b9b6d736878aa652310b0295a001c26fc1c5ec1
justcow <68345611+JustCoww@users.noreply.github.com>
2022-03-26 19:24:14 +0000

    Update extras.py

--- slowedvideos/extras.py
+++ slowedvideos/extras.py
@@ -3,18 +3,18 @@ def buildvideo(song, artist, audiomode, audio, covermode, cover, toptext, export
     # Importing modules
     from slowedvideos.video import makevideo, makethumb, exportvideo
     from slowedvideos.audio import makeslowed, downloadurl
+    from os import mkdir, chdir, system, remove
     from multiprocessing import Process
     from shutil import copyfileobj
-    from os import mkdir, chdir
     from requests import get
     from shutil import copy
     from time import sleep
 
     # Setting up file names
-    video_export = f'videofile {song}.mov'
+    video_export = f'videofile {song}.mp4'
     thumb_output = f'thumb {song}.png'
     video_output = f'video {song}.png'
-    slowd_output = f'slowed {song}.wav'
+    slowd_output = f'slowed {song}.mp3'
     folder = f'slowed {song}'
     mkdir(folder)
 
@@ -43,11 +43,14 @@ def buildvideo(song, artist, audiomode, audio, covermode, cover, toptext, export
     chdir(folder)
 
     # Building the video
-    slow_cmd = Process(target=makeslowed, args=[audio, speed, slowd_output])
+    temp_slowd_output = slowd_output + '.wav'
+    slow_cmd = Process(target=makeslowed, args=[audio, speed, temp_slowd_output])
     mkthumb_cmd = Process(target=makethumb, args=[cover, thumb_output])
 
     slow_cmd.start()
     mkthumb_cmd.start()
+    system(f'ffmpeg -i "{temp_slowd_output}" -vn -ar 44100 -ac 2 -b:a 320k "{slowd_output}"')
+    remove(temp_slowd_output)
     makevideo(cover, song, artist, toptext, video_output)
     exportvideo(slowd_output, video_output, exportmode, video_export)
 
